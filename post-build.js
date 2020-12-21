6f6bb15948f68a5ee00dfc6ca67eb668d249f590
崔 明辉 <cuis@vip.qq.com>
2020-12-21 22:52:32 +0800

    chore: Use relative path.

--- post-build.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const code = fs.readFileSync("./build/index.html", { encoding: "utf-8" });
-const newCode = code.replace(/\"\//gi, '"./');
-fs.writeFileSync("./build/index.html", newCode);;
