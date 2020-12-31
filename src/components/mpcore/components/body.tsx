8d4e240a072b14eaffd23ac5a8b84271f1b1e63a
崔 明辉 <cuis@vip.qq.com>
2020-12-31 11:40:23 +0800

    fix: Page overflowX should be hidden.

--- src/components/mpcore/components/body.tsx
+++ src/components/mpcore/components/body.tsx
@@ -6,11 +6,11 @@ export class Body extends Component<{ data: any }> {
   render() {
     if (this.props.data.isListBody) {
       return (
-        <div style={{ width: "100%" }}>{MPCore.render(this.props.data)}</div>
+        <div style={{ width: "100%", overflowX: "hidden" }}>{MPCore.render(this.props.data)}</div>
       );
     } else {
       return (
-        <div style={{ width: "100%", height: "100%" }}>
+        <div style={{ width: "100%", height: "100%", overflowX: "hidden" }}>
           {MPCore.render(this.props.data)}
         </div>
       );
