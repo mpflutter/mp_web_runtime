503ac94ba345c554ad0790c4b773a70ab2f151a9
崔 明辉 <cuis@vip.qq.com>
2021-01-08 20:52:38 +0800

    fix: ListView wrapped.

--- src/components/mpcore/components/list_view.tsx
+++ src/components/mpcore/components/list_view.tsx
@@ -1,26 +1,35 @@
 import { Component } from "react";
 import { MPComponentsProps } from "../component";
 import { cssPadding } from "../utils/geometry";
+import { DivContextConsumer } from "./div_context";
 
 export class ListView extends Component<{ data: MPComponentsProps }> {
   render() {
     return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection:
-            this.props.data.attributes.scrollDirection === "Axis.horizontal"
-              ? "row"
-              : "column",
-          justifyContent: "flex-start",
-          alignItems: "stretch",
-          minWidth: this.props.data.attributes.scrollDirection !== "Axis.horizontal" ? "100%" : "unset",
-          minHeight: this.props.data.attributes.scrollDirection === "Axis.horizontal" ? "100%" : "unset",
-          ...cssPadding(this.props.data.attributes.padding),
-        }}
-      >
-        {this.props.children}
-      </div>
+      <DivContextConsumer>
+        <div
+          style={{
+            display: "flex",
+            flexDirection:
+              this.props.data.attributes.scrollDirection === "Axis.horizontal"
+                ? "row"
+                : "column",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            minWidth:
+              this.props.data.attributes.scrollDirection !== "Axis.horizontal"
+                ? "100%"
+                : "unset",
+            minHeight:
+              this.props.data.attributes.scrollDirection === "Axis.horizontal"
+                ? "100%"
+                : "unset",
+            ...cssPadding(this.props.data.attributes.padding),
+          }}
+        >
+          {this.props.children}
+        </div>
+      </DivContextConsumer>
     );
   }
 }
