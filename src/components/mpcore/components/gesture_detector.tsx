150e98cf4b0621950d9dade94785f0a03d4de6b9
崔 明辉 <cuis@vip.qq.com>
2021-04-13 11:50:30 +0800

    feat: Add touch opacity feature to GestureDetector. feat: Make overlay zIndex larger.

--- src/components/mpcore/components/gesture_detector.tsx
+++ src/components/mpcore/components/gesture_detector.tsx
@@ -4,10 +4,26 @@ import { App } from "../../app";
 import { MPComponentsProps } from "../component";
 
 export class GestureDetector extends Component<{ data: MPComponentsProps }> {
+  state = { touching: false };
+
   render() {
     return (
       <div
-        style={{}}
+        className={this.state.touching ? "gesture_detector_hover" : ""}
+        onTouchStart={() => {
+          this.setState({ touching: true });
+        }}
+        onTouchMove={() => {
+          this.setState({ touching: false });
+        }}
+        onTouchEnd={() => {
+          setTimeout(() => {
+            this.setState({ touching: false });
+          }, 150);
+        }}
+        onTouchCancel={() => {
+          this.setState({ touching: false });
+        }}
         onClick={
           this.props.data.attributes.onTap
             ? (e: any) => {
--- src/components/mpcore/components/overlay.tsx
+++ src/components/mpcore/components/overlay.tsx
@@ -17,7 +17,7 @@ export class Overlay extends Component<{
           left: "0px",
           right: "0px",
           bottom: "0px",
-          zIndex: 1,
+          zIndex: 3,
           backgroundColor: cssColor(this.props.data.attributes.backgroundColor),
         }}
         onClick={(e: any) => {
